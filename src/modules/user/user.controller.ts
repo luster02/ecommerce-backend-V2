a72467ed0a7862d35be8a5ef388f8f6de961c829
luster02 <bombolti_02@hotmail.com>
2020-07-19 00:19:39 -0500

    [ADD]: http code & custom response

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,37 +1,37 @@
-import { Controller, Get, Param, Post, Body, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, ParseIntPipe, Patch, HttpCode } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { UserDetails } from './user.detail.entity';
+import { CustomResponse } from '../../interfaces/Response.interface';
 
 @Controller('users')
 export class UserController {
     constructor(private readonly _userService: UserService) { }
 
     @Get(':id')
-    async getUser(@Param('id', ParseIntPipe) id: number) {
+    @HttpCode(200)
+    async getUser(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
         const user = await this._userService.get(id)
         return { ok: true, data: user }
     }
 
     @Get()
-    async getAllUsers() {
+    @HttpCode(200)
+    async getAllUsers(): Promise<CustomResponse> {
         const users = await this._userService.getAll()
         return { ok: true, data: users }
     }
 
-    @Post('')
-    async createUser(@Body() body: User) {
-        const user = await this._userService.create(body)
-        return { ok: true, data: user }
-    }
-
-    @Post(':id')
-    async editUser(@Param('id', ParseIntPipe) id: number, @Body() body: User) {
+    @Patch(':id')
+    @HttpCode(200)
+    async editUser(@Param('id', ParseIntPipe) id: number, @Body() body: UserDetails): Promise<CustomResponse> {
         await this._userService.update(id, body)
         return { ok: true, data: 'updated' }
     }
 
     @Delete(':id')
-    async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    @HttpCode(200)
+    async deleteUser(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
         await this._userService.delete(id)
         return { ok: true, data: 'deleted' }
     }
