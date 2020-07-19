d016dbe0c8205cc3d465cdd0c19b5467bc9f110f
luster02 <bombolti_02@hotmail.com>
2020-07-19 00:18:32 -0500

    [ADD]: status code & custom response

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,37 +1,43 @@
-import { Controller, Get, Param, Post, Body, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, ParseIntPipe, HttpCode } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { Role } from './role.entity';
+import { CustomResponse } from 'src/interfaces/Response.interface';
 
 @Controller('roles')
 export class RoleController {
     constructor(private readonly _roleService: RoleService) { }
 
     @Get(':id')
-    async getRole(@Param('id', ParseIntPipe) id: number) {
+    @HttpCode(200)
+    async getRole(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
         const role = await this._roleService.get(id)
         return { ok: true, data: role }
     }
 
     @Get()
-    async getAllRoles() {
+    @HttpCode(200)
+    async getAllRoles(): Promise<CustomResponse> {
         const roles = await this._roleService.getAll()
         return { ok: true, data: roles }
     }
 
     @Post('')
-    async createRole(@Body() body: Role) {
+    @HttpCode(200)
+    async createRole(@Body() body: Role): Promise<CustomResponse> {
         const role = await this._roleService.create(body)
         return { ok: true, data: role }
     }
 
     @Post(':id')
-    async editRole(@Param('id', ParseIntPipe) id: number, @Body() body: Role) {
+    @HttpCode(201)
+    async editRole(@Param('id', ParseIntPipe) id: number, @Body() body: Role): Promise<CustomResponse> {
         await this._roleService.update(id, body)
         return { ok: true, data: 'updated' }
     }
 
     @Delete(':id')
-    async deleteRole(@Param('id', ParseIntPipe) id: number) {
+    @HttpCode(200)
+    async deleteRole(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
         await this._roleService.delete(id)
         return { ok: true, data: 'deleted' }
     }
