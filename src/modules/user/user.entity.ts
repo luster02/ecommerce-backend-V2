4d854340069f8e3362490c4d84f06db2671c1785
luster02 <bombolti_02@hotmail.com>
2020-07-19 00:21:33 -0500

    [FIX]: delete cascade & remove prop

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -7,25 +7,18 @@ export class User extends BaseEntity {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
-    @Column({ type: 'varchar', unique: true, length: 25, nullable: false })
-    username: string;
-
     @Column({ type: 'varchar', nullable: false })
     email: string;
 
     @Column({ type: 'varchar', nullable: false })
     password: string;
 
-    @OneToOne(type => UserDetails, { cascade: true, nullable: false, eager: true })
-    @JoinColumn({ name: 'detail_id' })
+    @OneToOne(type => UserDetails, userDetails => userDetails.user, { cascade: true, nullable: false, eager: true, onDelete: 'CASCADE' })
     details: UserDetails;
 
-    @ManyToMany(type => Role, role => role.users, { eager: true })
+    @ManyToMany(type => Role, role => role.users, { eager: true, onDelete: 'CASCADE' })
     @JoinTable({ name: 'user_roles' })
     roles: Role[];
-
-    @Column({ type: 'varchar', default: 'ACTIVE', length: 8 })
-    status: string;
 
     @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
     createdAt: Date;
