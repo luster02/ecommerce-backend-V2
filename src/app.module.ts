c3af09dddbb98c3b3bc23b7429269b173c28096d
luster02 <bombolti_02@hotmail.com>
2020-07-19 21:19:00 -0500

    [ADD]: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { Configuration } from './config/config.key';
@@ -8,11 +6,13 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
 import { RoleModule } from './modules/role/role.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ShopModule } from './modules/shop/shop.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UserModule, RoleModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule, DatabaseModule, UserModule, RoleModule, AuthModule, ShopModule, ProductsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {
   static port: number | string;
