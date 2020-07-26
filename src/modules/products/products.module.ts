703c1ce70ab1aa649e160d9bbfabf93dfd36ead7
luster02 <bombolti_02@hotmail.com>
2020-07-26 01:03:56 -0500

    [ADD]: repository

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductRepository } from './products.repository'
 import { ProductService } from './products.service';
 import { ProductsController } from './products.controller';
+import { AssetRepository } from '../asset/asset.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductRepository])],
+  imports: [TypeOrmModule.forFeature([ProductRepository, AssetRepository])],
   providers: [ProductService],
   controllers: [ProductsController]
 })
