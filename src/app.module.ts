e3410ca79c80de1cf24db5d176fbebc1f64f0dba
luster02 <bombolti_02@hotmail.com>
2020-08-05 19:10:58 -0500

    [ADD]: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,24 @@ import { Configuration } from './config/config.key';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
 import { RoleModule } from './modules/role/role.module';
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth/user/auth.module';
 import { ShopModule } from './modules/shop/shop.module';
 import { ProductsModule } from './modules/products/products.module';
 import { GalleryModule } from './modules/gallery/gallery.module';
 import { AssetModule } from './modules/asset/asset.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CartModule } from './modules/cart/cart.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { CustomerAuthModule } from './modules/auth/customer/customer.auth.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UserModule, RoleModule, AuthModule, ShopModule, ProductsModule, GalleryModule, AssetModule, CloudinaryModule],
+  imports: [
+    ConfigModule, DatabaseModule, UserModule,
+    RoleModule, AuthModule, ShopModule,
+    ProductsModule, GalleryModule, AssetModule,
+    CloudinaryModule, CartModule, CustomerModule,
+    CustomerAuthModule
+  ],
   controllers: [],
   providers: [],
 })
